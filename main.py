cfda450d1e05bcd096826e9535506617acb26b89
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-17 05:59:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -359,14 +359,14 @@ def onmessage(update,bot:ObigramClient):
             msg_start = 'Buscando proxy, esto puede tardar de una a dos horas...'
             bot.sendMessage(update.message.chat.id,msg_start)
             print("Buscando proxy...")
-            for port in range(8080):
+            for port in range(3127,3130):
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-                result = sock.connect_ex(('152.206.85.87',port))  
+                result = sock.connect_ex(('152.206.139.117',port))  
 
                 if result == 0: 
                     print ("Puerto abierto!")
                     print (f"Puerto: {port}")  
-                    proxy = f'152.206.85.87:{port}'
+                    proxy = f'152.206.139.117:{port}'
                     proxy_new = S5Crypto.encrypt(f'{proxy}')
                     msg = 'Su nuevo proxy es:\n\nsocks5://' + proxy_new
                     bot.sendMessage(update.message.chat.id,msg)
